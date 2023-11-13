e628b5535c786b6fe6ac3908e45649a1407de8da
saqlainmusi <rakibshaharier96@gmail.com>
2023-11-13 19:17:03 +0600

    sql file has been added

---
+++ contact-list.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2023 at 02:09 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `contact-list-2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_info_table`
+--
+
+CREATE TABLE `contact_info_table` (
+  `id` int(20) NOT NULL,
+  `user_id` int(20) NOT NULL,
+  `contact_image` varchar(300) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `company` varchar(50) NOT NULL,
+  `job_title` varchar(100) NOT NULL,
+  `department` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `street_address` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `postal_code` varchar(20) NOT NULL,
+  `birth_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_table`
+--
+
+CREATE TABLE `login_table` (
+  `id` int(20) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact_info_table`
+--
+ALTER TABLE `contact_info_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_table`
+--
+ALTER TABLE `login_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact_info_table`
+--
+ALTER TABLE `contact_info_table`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `login_table`
+--
+ALTER TABLE `login_table`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
