72ce900773499f3c5dcfea84111a764f6ffd02c6
saqlainmusi <rakibshaharier96@gmail.com>
2023-11-21 11:22:13 +0600

    server url and mail configuration has been  changed

--- reset_verification.php
+++ reset_verification.php
@@ -17,33 +17,18 @@
   $mail = new PHPMailer(true);
   
   try {
-      //Server settings
-      // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-      $mail->isSMTP();                                            //Send using SMTP
-      $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-      $mail->Username   = 'affordable000@gmail.com';                     //SMTP username
-      $mail->Password   = 'tsjtmikularmseaf';                               //SMTP password
-      // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-      $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+      $mail->isSMTP();                                            
+      $mail->Host       = 'localhost';                   
+      $mail->Port       = 1025;                                   
   
-      //Recipients
-      $mail->setFrom('affordable000@gmail.com', 'Mailer');
-      $mail->addAddress($email);     //Add a recipient
-      // $mail->addAddress('ellen@example.com');               //Name is optional
-      // $mail->addReplyTo('info@example.com', 'Information');
-      // $mail->addCC('cc@example.com');
-      // $mail->addBCC('bcc@example.com');
-  
-      //Attachments
-      // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-      // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-  
-      //Content
-      $mail->isHTML(true);                                  //Set email format to HTML
+     
+      $mail->setFrom('sender@example.com', 'Contact-list Mailer');
+      $mail->addAddress($email);     
+   
+      $mail->isHTML(true);                                
       $mail->Subject ="Reset Code";
       $mail->Body    = $reset_code;
-      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+      $mail->AltBody = 'Use this six digit code to verify your account';
   
       $mail->send();
       echo 'Message has been sent';
