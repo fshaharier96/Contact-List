43840bfc35411b703ece5b4d69e48ad022be2b9a
Sabuj Kundu <manchumahara@gmail.com>
2023-11-29 10:07:26 +0600

    suggestion added

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,33 +1,50 @@
+(function ($) {
+    'use strict';
 
-$("#signupForm").validate({
-    rules: {
 
-        email:{
-            required:true
-        },
-        username: {
-            required: true,
-            // minlength: 3
-        },
-     
-        password: {
-            required: true,
-            // minlength: 6
-        }
-    },
-    messages: {
 
-        email:{
-           required:"Please enter valid email"
-        },
-        username: {
-            required: "Please enter a username",
-            // minlength: "Username must be at least 3 characters long"
-        },
-    
-        password: {
-            required: "Please enter a password",
-            // minlength: "Password must be at least 8 characters long"
-        }
-    }
-});
+    console.log("I am ready: before");
+
+    $(document).ready(function () {
+
+        console.log("I am ready");
+
+        $("#signupForm").validate({
+            debug: true,
+            rules: {
+
+                email:{
+                    required:true
+                },
+                username: {
+                    required: true,
+                    // minlength: 3
+                },
+
+                password: {
+                    required: true,
+                    // minlength: 6
+                }
+            },
+            messages: {
+
+                email:{
+                    required:"Please enter valid email"
+                },
+                username: {
+                    required: "Please enter a username",
+                    // minlength: "Username must be at least 3 characters long"
+                },
+
+                password: {
+                    required: "Please enter a password",
+                    // minlength: "Password must be at least 8 characters long"
+                }
+            },
+            //errorClass: "invalid-feedback",
+            errorElement: "p"
+        });
+    });
+
+}(jQuery));
+
--- signup.php
+++ signup.php
@@ -1,92 +1,23 @@
 <?php
- require('vendor/autoload.php');
- use Rakit\Validation\Validator;
- include_once "classes/database.php";
- $db_connect=new Database();
- $conn=$db_connect->conn;
+require( 'vendor/autoload.php' );
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+use Rakit\Validation\Validator;
 
-    <!--fontawsome cdn link-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
-     <!--boostrap css file-->
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-
-    <!--styling file-->
-    <link rel="stylesheet" href="style/css/main.css">
-
-    <title>Document</title>
-</head>
-<body>
-
-<!--bootstrap code  starts-->
-
-<div class="container-fluid h-auto p-3">
-    <div class="row d-flex justify-content-center align-items-center flex-column  p-3">
-
-     <div class="login-logo col-3 my-3">
-        <i class="fa-solid fa-address-card"></i>
-     </div>
-
-     <div class="mb-2 mt-5 col-3">
-        <h3 class="text-center">Registration</h3>
-     </div>
-
-     <div class="col-3 custom-col-height px-4 py-3 mb-5 shadow background"> 
-          <form id="signupForm" action="" method="post">
-               <div class="mb-3 mt-3">
-                  <label class="form-label">Email</label>
-                  <input type="text" name="email" class="form-control border border-secondary">
-                </div>
-
-                <div class="mb-3 mt-3">
-                  <label class="form-label">Username</label>
-                  <input type="text" name="username" class="form-control border border-secondary">
-                </div>
-                <div class="mb-4">
-                  <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" name="password" class="form-control border border-secondary">
-                </div>
-                <div class="mb-3 form-check">
-                  <input type="checkbox" class="form-check-input border border-black" id="exampleCheck1">
-                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" name="submit" value="Login" class="btn btn-primary form-control">Register</button>
-            </form>
-      </div>
-      <div class="col-3 mt-3 border border-secondary rounded-2  p-2 background1">
-        <h6  class="text-center fw-semibold text-primary">
-           Sign in with key pass
-        </h6>
-        <p class="text-center"><span>Have already registered?</span><a href="index.php">Log into account</a></p>
-
-      </div>
-    </div>
-</div>
+include_once "classes/database.php";
+$db_connect = new Database();
+$conn       = $db_connect->conn;
 
 
+//do the validation check and error check or any kind of database operation before any html is rendered
 
-<!--bootstrap code ends-->
-
-
-
-
-
-<?php
 
 // if(isset($_POST['submit'])){
 
 //     $email=$_POST['email'];
 //     $username= $_POST['username'];
 //     $password= $_POST['password'];
-    
-  
+
+
 //   if(!empty($username) && !empty($password) && !empty($email))
 //   {
 
@@ -95,7 +26,7 @@
 //    $result=mysqli_query($conn,$sql) or die("query unsuccesful");
 //    if($result)
 //    {
-    
+
 //     if(mysqli_num_rows($result)==0)
 //     {
 //          if(mysqli_query($conn,$sql1))
@@ -110,72 +41,130 @@
 //      else{
 //             echo "This account already exist";
 //          }
- 
+
 //       }
 //    }
 //     else{
 //         echo "<p>input fields are empty!</p>";
 //      }
-     
+
 //  }
- 
- ?>
 
-<?php 
- $validator = new Validator();
- $validation = $validator->validate($_POST, [
-  'email'=> 'required',
-  'username'=>'required',
-  'password' => 'required'
-]);
+?>
 
-$submit_msg="";
+<?php
+$validator  = new Validator();
+$validation = $validator->validate( $_POST, [
+	'email'    => 'required',
+	'username' => 'required',
+	'password' => 'required'
+] );
 
-if ($validation->fails()) {
+$submit_msg = "";
+
+if ( $validation->fails() ) {
 // handling errors
 // $errors = $validation->errors();
 // echo "<pre>";
 // print_r($errors->firstOfAll());
 // echo "</pre>";
 // echo "<h1>You have input invalid username or password</h1>";
-$submit_msg="Invalid data is  entered";
-echo $submit_msg;
+	$submit_msg = "Invalid data is  entered";
+	echo $submit_msg;
 
 } else {
 // validation passes
-   
-    $email=$_POST['email'];
-    $username= $_POST['username'];
-    $password= $_POST['password'];
-    $hashedPassword=hash('sha256',$password);
 
-   $sql="SELECT * FROM login_table WHERE email='{$email}'";
-   $sql1="INSERT INTO login_table(email,username,password) VALUES('{$email}','{$username}','{$hashedPassword}')";
-   $result=mysqli_query($conn,$sql) or die("query unsuccesful");
-   if($result)
-   {
-    
-    if(mysqli_num_rows($result)==0)
-    {
-         if(mysqli_query($conn,$sql1))
-        {
-             header("Location:{$host}");
-         }
+	$email          = $_POST['email'];
+	$username       = $_POST['username'];
+	$password       = $_POST['password'];
+	$hashedPassword = hash( 'sha256', $password );
 
-        else{
-                echo "<p>Register failed! Try again</p>";
-            }
-     }
-     else{
-            echo "This email address has already been taken";
-         }
- 
-      }
-   }
+	$sql  = "SELECT * FROM login_table WHERE email='{$email}'";
+	$sql1 = "INSERT INTO login_table(email,username,password) VALUES('{$email}','{$username}','{$hashedPassword}')";
+	$result = mysqli_query( $conn, $sql ) or die( "query unsuccesful" );
+	if ( $result ) {
 
- 
- 
- ?>
+		if ( mysqli_num_rows( $result ) == 0 ) {
+			if ( mysqli_query( $conn, $sql1 ) ) {
+				header( "Location:{$host}" );
+			} else {
+				echo "<p>Register failed! Try again</p>";
+			}
+		} else {
+			echo "This email address has already been taken";
+		}
+
+	}
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--fontawsome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!--boostrap css file-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+    <!--styling file-->
+    <link rel="stylesheet" href="style/css/main.css">
+
+    <title>CRM - Signup</title>
+</head>
+<body>
+
+<!--bootstrap code  starts-->
+
+<div class="container-fluid h-auto p-3">
+    <div class="row d-flex justify-content-center align-items-center flex-column  p-3">
+        <div class="login-logo col-3 my-3">
+            <i class="fa-solid fa-address-card"></i>
+        </div>
+        <div class="mb-2 mt-5 col-3">
+            <h3 class="text-center">Registration</h3>
+        </div>
+        <div class="col-3 custom-col-height px-4 py-3 mb-5 shadow background">
+
+            <form id="signupForm" action="" method="post">
+                <div class="mb-3 mt-3">
+                    <label for="email_field" class="form-label">Email</label>
+                    <input id="email_field" type="text" name="email" class="form-control border border-secondary" />
+                </div>
+                <div class="mb-3 mt-3">
+                    <label for="username_field" class="form-label">Username</label>
+                    <input id="username_field" type="text" name="username" class="form-control border border-secondary" />
+                </div>
+                <div class="mb-4">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control border border-secondary" />
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input border border-black" name="agree_terms" id="agree_terms" />
+                    <label class="form-check-label" for="agree_terms">Check me out</label>
+                </div>
+                <button type="submit" name="submit" value="Login" class="btn btn-primary form-control">Register</button>
+            </form>
+        </div>
+        <div class="col-3 mt-3 border border-secondary rounded-2  p-2 background1">
+            <h6 class="text-center fw-semibold text-primary">
+                Sign in with key pass
+            </h6>
+            <p class="text-center"><span>Have already registered?</span><a href="index.php">Log into account</a></p>
+        </div>
+    </div>
+</div>
+
+
+<!--bootstrap code ends-->
+
+
 
 
 
@@ -183,14 +172,11 @@ echo $submit_msg;
 <script src="assets/js/jquery.js"></script>
 <script src="assets/vendors/jquery-form-validation/jquery.validate.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/signup.js"></script>
-  <script>
-      if(window.history.replaceState){
-         window.history.replaceState(null,null,window.location.href);
-         }
-     </script>
-
-
-    
+<script src="assets/js/signup.js?v=1"></script>
+<!--<script>
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+</script>-->
 </body>
 </html>
