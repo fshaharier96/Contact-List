ceff1684c69faa9020b2e2b709e9298937328a67
saqlainmusi <rakibshaharier96@gmail.com>
2023-11-30 13:24:28 +0600

    sign up page updated

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -5,68 +5,68 @@
 
     // console.log("I am ready: before");
 
-    $(document).ready(function () {
-
-        // console.log("I am ready");
-
-        $("#signupForm").validate({
-
-            rules: {
-
-                email:{
-                    required:true,
-                    email:true,
-                },
-                username: {
-                    required: true,
-                    maxlength:15,
-                    minlength:5
-                },
-
-                password: {
-                    required: true,
-                    minlength: 6,
-                    maxlength: 12
-                },
-                agree_terms:{
-                    required: true
-                }
-
-            },
-            messages: {
-
-                email:{
-                    required:"Please enter your email",
-                    email:"please enter a valid email address"
-                },
-                username: {
-                    required: "Please enter a username",
-                    maxlength: "character length exceeds maxlength",
-                    minlength: "Too few character"
-                },
-
-                password: {
-                    required: "Please enter a password",
-                    maxlength: "character length exceeds maxlength",
-                    minlength: "Too few character"
-                    // minlength: "Password must be at least 8 characters long"
-                },
-                agree_terms:{
-                    required:"Please check the box to agree to the terms and conditions"
-                }
-            },
-
-            errorPlacement: function(error, element) {
-                if (element.attr('name') === 'agree_terms') {
-                    error.insertAfter('#check-label-id');
-
-                }
-            }
-
-            //errorClass: "invalid-feedback",
-            // errorElement: "p"
-        });
-    });
+    // $(document).ready(function () {
+    //
+    //     // console.log("I am ready");
+    //
+    //     $("#signupForm").validate({
+    //
+    //         rules: {
+    //
+    //             email:{
+    //                 required:true,
+    //                 email:true,
+    //             },
+    //             username: {
+    //                 required: true,
+    //                 maxlength:15,
+    //                 minlength:5
+    //             },
+    //
+    //             password: {
+    //                 required: true,
+    //                 minlength: 6,
+    //                 maxlength: 12
+    //             },
+    //             agree_terms:{
+    //                 required: true
+    //             }
+    //
+    //         },
+    //         messages: {
+    //
+    //             email:{
+    //                 required:"Please enter your email",
+    //                 email:"please enter a valid email address"
+    //             },
+    //             username: {
+    //                 required: "Please enter a username",
+    //                 maxlength: "character length exceeds maxlength",
+    //                 minlength: "Too few character"
+    //             },
+    //
+    //             password: {
+    //                 required: "Please enter a password",
+    //                 maxlength: "character length exceeds maxlength",
+    //                 minlength: "Too few character"
+    //                 // minlength: "Password must be at least 8 characters long"
+    //             },
+    //             agree_terms:{
+    //                 required:"Please check the box to agree to the terms and conditions"
+    //             }
+    //         },
+    //
+    //         errorPlacement: function(error, element) {
+    //             if (element.attr('name') === 'agree_terms') {
+    //                 error.insertAfter('#check-label-id');
+    //
+    //             }
+    //         }
+    //
+    //         //errorClass: "invalid-feedback",
+    //         // errorElement: "p"
+    //     });
+    // });
 
 }(jQuery));
 
