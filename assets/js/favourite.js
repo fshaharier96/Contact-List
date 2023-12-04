ab62a40bdb8ab26e66fb0e6b9bd7d2eb5a40e3da
saqlainmusi <rakibshaharier96@gmail.com>
2023-12-04 09:56:07 +0600

    favourite.js has been updated

--- assets/js/favourite.js
+++ assets/js/favourite.js
@@ -1,23 +1,30 @@
+(function ($) {
+    'use strict';
+
+    $(document).ready(function () {
+
+        $(document).ready(function () {
+            $('tr td button').click(function () {
+                // alert("button is clicked")
+                let favourite_id = $(this).data("favour");
+                $.ajax({
+                    url: "favourite.php",
+                    type: 'post',
+                    data: {row_id: favourite_id, favourite: 0},
+                    success: function (response) {
+                        if (response == 1) {
+                            window.location = "favourite_data.php";
+
+                        }
 
 
-$(document).ready(function(){
-$('tr td button').click(function(){
-  // alert("button is clicked")
-    let favourite_id=$(this).data("favour");
-    $.ajax({
-      url:"favourite.php",
-      type:'post',
-      data:{row_id:favourite_id,favourite:0},
-      success:function(response){ 
-        if(response==1){
-          window.location="favourite_data.php";
-          
-        }
-        
-       
-      }
-    })
+                    }
+                })
 
-})
+            })
 
-})
+        })
+
+    });
+
+}(jQuery));
